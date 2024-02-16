6cbde309771271605feb5d93981d1e974de8ab87
Arisevilla <arisevilla93@gmail.com>
2024-02-16 11:03:23 -0300

    models crud

---
+++ app/models/mainModel.php
@@ -0,0 +1,116 @@
+<?php
+    namespace app\models;
+    use \PDO;
+
+    if(file_exists(__DIR__."/../../config/server.php")){
+        require_once __DIR__."/../../config/server.php";
+    }
+
+    class mainModel{
+
+        private $server= DB_SERVER;
+        private $db= DB_NAME;
+        private $user= DB_USER;
+        private $pass= DB_PASS;
+
+        protected function conectar(){
+            $conexion = new PDO("mysql:host=".$this->server.";dbname=".$this->db, $this->user, $this->pass);
+            $conexion->exec("SET CHARACTER SET uft8");
+            return $conexion;
+        }
+
+        protected function ejecutarConsulta($consulta){
+            $sql=$this->conectar()->prepare($consulta);
+            $sql->execute();
+            return $sql;
+        }
+
+        public function limpiarCadena($cadena){
+            $palabras=["<script>","</script>","<script src","<script type=","SELECT * FROM","SELECT "," SELECT ","DELETE FROM","INSERT INTO","DROP TABLE","DROP DATABASE","TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES","<?php","?>","--","^","<",">","==","=",";","::"];
+
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            foreach($palabras as $palabra){
+                $cadena=str_ireplace($palabra, "",$cadena);
+                
+            }
+
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            return $cadena;
+        }
+
+        protected function verificarDatos($filtro,$cadena){
+            if(preg_match("/^".$filtro."$/", $cadena)){
+                return false;
+            }else{
+                return true;
+            }
+
+        }
+
+        protected function guardarDatos($tabla,$datos){
+            $query="INSERT INTO $tabla (";
+
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){ $query.=","; }
+                $query.=$clave["campo_nombre"];
+                $C++;
+            }
+
+            $query.=") VALUES (";
+
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){ $query.=","; }
+                $query.=$clave["campo_marcador"];
+                $C++;
+            }
+
+            $query.=")";
+            $sql=$this->conectar()->prepare($query);
+
+            foreach($datos as $clave){
+                $sql->bindParam($clave["campo_marcador"],$clave["campo_valor"]);
+            }
+
+            $sql->execute();
+
+            return $sql;
+
+        }
+
+        public function seleccionarDatos($tipo,$tabla,$campo,$id){
+            $tipo=$this->limpiarCadena($tipo);
+            $tabla=$this->limpiarCadena($tabla);
+            $campo=$this->limpiarCadena($campo);
+            $id=$this->limpiarCadena($id);
+
+            if($tipo=="Unico"){
+                $sql=$this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo=:ID");
+                $sql->bindParam(":ID",$id);
+            }elseif($tipo=="Normal"){
+                $sql=$this->conectar()->prepare("SELECT $campo FROM $tabla");
+            }
+
+            $sql->execute();
+
+            return $sql;
+        }
+
+        protected function actualizarDatos($tabla,$datos,$condicion){
+
+            $query="UPDATE $tabla SET ";
+
+            $C=0;
+            foreach($datos as $clave){
+                if($C>=1){ $query.=",";}
+                $query.=$clave["campo_nombre"];
+                $C++;
+            }
+
+        }
+    }
---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL="http://localhost/CRUD_PHP/";
+    const APP_NAME="CRUD PHP";
+    const APP_SESSION_NAME="CRUD";
+
+    date_default_timezone_set("America/Santiago");
